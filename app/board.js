0a113c83036f79e4bf599beb04858e2f615486cc
Jay Gruber <jaydgruber@gmail.com>
2016-05-26 16:34:46 -0500

    finished board basic structure. basic toString function for console debugging working on a checkWin function

--- app/board.js
+++ app/board.js
@@ -30,3 +30,26 @@ Board.prototype.setCell = function(x, y, value){
 	var row = this.getRow(y);
 	row[x] = value;
 }
+
+//inserts value {{player}} onto lowest nonempty cell in {{column}}
+//returns true if success, false if column is full
+Board.prototype.insertIntoColumn = function(column, player){
+	var x = column;
+	for(y = this.size - 1; y >= 0; y--){
+		if(this.getCell(x,y) == 0){
+			this.setCell(x, y, player);
+			return true;
+		}
+	}
+	return false;
+}
+
+Board.prototype.toString = function(){
+	for(y = 0; y < this.size; y++){
+		console.log(this.getRow(y));
+	}
+}
+
+Board.prototype.checkWin = function(){
+	
+}
--- tests/boardtest.js
+++ tests/boardtest.js
@@ -10,7 +10,7 @@ describe('boardtest', function(){
 	});
 	it('constructor sets size correctly', function(){
 		expect(testBoard.size).to.be.eql(SIZE);
-	})
+	});
 	it('createRow returns a row of correct size', function(){
 		var row = testBoard.createRow();
 		expect(row.length).to.be.eql(SIZE);
@@ -19,7 +19,7 @@ describe('boardtest', function(){
 		testBoard.board = [];
 		testBoard.initializeBoard();
 		expect(testBoard.board.length).to.be.eql(SIZE);
-	})
+	});
 	it('getRow returns correct row', function(){
 		testBoard.board = [
 			[0,0,0],
@@ -28,7 +28,7 @@ describe('boardtest', function(){
 		];
 		var row = testBoard.getRow(1);
 		expect(row).to.be.eql([1,0,0]);
-	})
+	});
 	it('getCell returns correct cell', function(){
 		testBoard.board = [
 			[0,0,0],
@@ -37,10 +37,26 @@ describe('boardtest', function(){
 		];
 		var cell = testBoard.getCell(1,1);
 		expect(cell).to.be.eql(1);
-	})
+	});
 	it('setCell sets a cell correctly', function(){
 		testBoard.board = [[0]];
 		testBoard.setCell(0,0,1);
 		expect(testBoard.getCell(0,0)).to.be.eql(1);
-	})
+	});
+	it('insertIntoColumn returns true/false correctly and sets value correctly', function(){
+		testBoard.board = [
+			[0,1,0],
+			[0,1,0],
+			[0,1,0]
+		];
+		testBoard.size = 3;
+		var success = testBoard.insertIntoColumn(1, 1);
+		expect(success).to.be.false;
+		var success = testBoard.insertIntoColumn(0, 1);
+		expect(testBoard.getCell(0,2)).to.be.eql(1);
+		expect(success).to.be.true;
+	});
+	it('testing toString', function(){
+		testBoard.toString();
+	});
 });
